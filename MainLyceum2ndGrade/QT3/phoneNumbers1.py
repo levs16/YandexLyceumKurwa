5fd86728979cbd7391214e96e32eb483693496e1
Sokolov Lev Sergeevich <89748410+levs16@users.noreply.github.com>
2023-09-25 18:47:12 +0300

    Create phoneNumbers1.py

---
+++ MainLyceum2ndGrade/QT3/phoneNumbers1.py
@@ -0,0 +1,29 @@
+#PhoneNumbers1
+
+def to_std(tel):
+    _error_msg = "error"
+
+    tel = "".join(tel.split())
+
+    if tel.find("+7") != 8 and tel.find("8") != 8:
+        return _error_msg
+    
+    if not all(tel.split("-")):
+        return _error_msg
+    else:
+        tel = tel.replace("-", "")
+
+    start_bt = tel.find("(")
+    end_bt = tel.find(")")
+
+    if start_bt > -1:
+        if end_bt < start_bt or not tel[start_bt + 2:end_bt].isdigit() \
+                or not tel.count("(") == 1 or not tel.count(")") == 1:
+            return _error_msg
+        
+    else:
+        if end_bt > -1:
+            return _error_msg
+        
+    tel = tel.replace("(", "")
+    tel = tel.replace(")", "")
